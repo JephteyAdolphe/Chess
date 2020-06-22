2dee4a91a7d0d5f2b174b55ff4943a502898796c
JephteyAdolphe <ja983@scarletmail.rutgers.edu>
2020-06-21 20:21:10 -0400

    Fixed pieces

--- src/jFiles/Controllers/boardController.java
+++ src/jFiles/Controllers/boardController.java
@@ -2,11 +2,14 @@ package jFiles.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+
+import java.util.HashMap;
 
 public class boardController {
     public GridPane board_grid;
-    public ToggleButton d0;
     public ToggleButton d1;
     public ToggleButton d2;
     public ToggleButton d3;
@@ -14,9 +17,6 @@ public class boardController {
     public ToggleButton d5;
     public ToggleButton d6;
     public ToggleButton d7;
-    public ToggleButton c0;
-    public ToggleButton b0;
-    public ToggleButton a0;
     public ToggleButton c1;
     public ToggleButton b1;
     public ToggleButton a1;
@@ -38,103 +38,128 @@ public class boardController {
     public ToggleButton c7;
     public ToggleButton b7;
     public ToggleButton a7;
+    public StackPane sp_a3;
+    public ToggleButton a8;
+    public ToggleButton b8;
+    public ToggleButton c8;
+    public ToggleButton d8;
+    public ToggleButton e8;
+    public ToggleButton f8;
+    public ToggleButton g8;
+    public ToggleButton h8;
+    public ToggleButton e7;
+    public ToggleButton e6;
+    public ToggleButton e5;
+    public ToggleButton e4;
+    public ToggleButton e3;
+    public ToggleButton e2;
+    public ToggleButton e1;
+    public ToggleButton f7;
+    public ToggleButton f6;
+    public ToggleButton f5;
+    public ToggleButton f4;
+    public ToggleButton f3;
+    public ToggleButton f2;
+    public ToggleButton g7;
+    public ToggleButton f1;
+    public ToggleButton g6;
+    public ToggleButton g5;
+    public ToggleButton g4;
+    public ToggleButton g3;
+    public ToggleButton g2;
+    public ToggleButton g1;
+    public ToggleButton h7;
+    public ToggleButton h6;
+    public ToggleButton h5;
+    public ToggleButton h4;
+    public ToggleButton h3;
+    public ToggleButton h2;
+    public ToggleButton h1;
+
+    private HashMap toggleStatus = new HashMap();
+
+    private void test() {
+        System.out.println(a6.isSelected());
+    }
 
     @FXML
     public void initialize() {
-        a0.getStylesheets().add("resources/views/css/stylesheet.css");
-        a0.getStyleClass().add("wrook");
 
         a1.getStylesheets().add("resources/views/css/stylesheet.css");
-        a1.getStyleClass().add("wknight");
-
         a2.getStylesheets().add("resources/views/css/stylesheet.css");
-        a2.getStyleClass().add("wbishop");
-
-        a3.getStylesheets().add("resources/views/css/stylesheet.css");
-        a3.getStyleClass().add("wqueen");
-
-        a4.getStylesheets().add("resources/views/css/stylesheet.css");
-        a4.getStyleClass().add("wking");
-
-        a5.getStylesheets().add("resources/views/css/stylesheet.css");
-        a5.getStyleClass().add("wbishop");
-
-        a6.getStylesheets().add("resources/views/css/stylesheet.css");
-        a6.getStyleClass().add("wknight");
-
         a7.getStylesheets().add("resources/views/css/stylesheet.css");
-        a7.getStyleClass().add("wrook");
-
-        b0.getStylesheets().add("resources/views/css/stylesheet.css");
-        b0.getStyleClass().add("wpawn");
-
+        a8.getStylesheets().add("resources/views/css/stylesheet.css");
         b1.getStylesheets().add("resources/views/css/stylesheet.css");
-        b1.getStyleClass().add("wpawn");
-
         b2.getStylesheets().add("resources/views/css/stylesheet.css");
-        b2.getStyleClass().add("wpawn");
-
-        b3.getStylesheets().add("resources/views/css/stylesheet.css");
-        b3.getStyleClass().add("wpawn");
-
-        b4.getStylesheets().add("resources/views/css/stylesheet.css");
-        b4.getStyleClass().add("wpawn");
-
-        b5.getStylesheets().add("resources/views/css/stylesheet.css");
-        b5.getStyleClass().add("wpawn");
-
-        b6.getStylesheets().add("resources/views/css/stylesheet.css");
-        b6.getStyleClass().add("wpawn");
-
         b7.getStylesheets().add("resources/views/css/stylesheet.css");
-        b7.getStyleClass().add("wpawn");
-
-        c0.getStylesheets().add("resources/views/css/stylesheet.css");
-        c0.getStyleClass().add("bpawn");
-
+        b8.getStylesheets().add("resources/views/css/stylesheet.css");
         c1.getStylesheets().add("resources/views/css/stylesheet.css");
-        c1.getStyleClass().add("bpawn");
-
         c2.getStylesheets().add("resources/views/css/stylesheet.css");
-        c2.getStyleClass().add("bpawn");
-
-        c3.getStylesheets().add("resources/views/css/stylesheet.css");
-        c3.getStyleClass().add("bpawn");
-
-        c4.getStylesheets().add("resources/views/css/stylesheet.css");
-        c4.getStyleClass().add("bpawn");
-
-        c5.getStylesheets().add("resources/views/css/stylesheet.css");
-        c5.getStyleClass().add("bpawn");
-
-        c6.getStylesheets().add("resources/views/css/stylesheet.css");
-        c6.getStyleClass().add("bpawn");
-
         c7.getStylesheets().add("resources/views/css/stylesheet.css");
-        c7.getStyleClass().add("bpawn");
-
-        d0.getStylesheets().add("resources/views/css/stylesheet.css");
-        d0.getStyleClass().add("brook");
-
+        c8.getStylesheets().add("resources/views/css/stylesheet.css");
         d1.getStylesheets().add("resources/views/css/stylesheet.css");
-        d1.getStyleClass().add("bknight");
-
         d2.getStylesheets().add("resources/views/css/stylesheet.css");
-        d2.getStyleClass().add("bbishop");
-
-        d3.getStylesheets().add("resources/views/css/stylesheet.css");
-        d3.getStyleClass().add("bqueen");
-
-        d4.getStylesheets().add("resources/views/css/stylesheet.css");
-        d4.getStyleClass().add("bking");
-
-        d5.getStylesheets().add("resources/views/css/stylesheet.css");
-        d5.getStyleClass().add("bbishop");
-
-        d6.getStylesheets().add("resources/views/css/stylesheet.css");
-        d6.getStyleClass().add("bknight");
-
         d7.getStylesheets().add("resources/views/css/stylesheet.css");
-        d7.getStyleClass().add("brook");
+        d8.getStylesheets().add("resources/views/css/stylesheet.css");
+        e1.getStylesheets().add("resources/views/css/stylesheet.css");
+        e2.getStylesheets().add("resources/views/css/stylesheet.css");
+        e7.getStylesheets().add("resources/views/css/stylesheet.css");
+        e8.getStylesheets().add("resources/views/css/stylesheet.css");
+        f1.getStylesheets().add("resources/views/css/stylesheet.css");
+        f2.getStylesheets().add("resources/views/css/stylesheet.css");
+        f7.getStylesheets().add("resources/views/css/stylesheet.css");
+        f8.getStylesheets().add("resources/views/css/stylesheet.css");
+        g1.getStylesheets().add("resources/views/css/stylesheet.css");
+        g2.getStylesheets().add("resources/views/css/stylesheet.css");
+        g7.getStylesheets().add("resources/views/css/stylesheet.css");
+        g8.getStylesheets().add("resources/views/css/stylesheet.css");
+        h1.getStylesheets().add("resources/views/css/stylesheet.css");
+        h2.getStylesheets().add("resources/views/css/stylesheet.css");
+        h7.getStylesheets().add("resources/views/css/stylesheet.css");
+        h8.getStylesheets().add("resources/views/css/stylesheet.css");
+
+        a2.getStyleClass().add("wpawn");
+        a7.getStyleClass().add("bpawn");
+        b2.getStyleClass().add("wpawn");
+        b7.getStyleClass().add("bpawn");
+        c2.getStyleClass().add("wpawn");
+        c7.getStyleClass().add("bpawn");
+        d2.getStyleClass().add("wpawn");
+        d7.getStyleClass().add("bpawn");
+        e2.getStyleClass().add("wpawn");
+        e7.getStyleClass().add("bpawn");
+        f2.getStyleClass().add("wpawn");
+        f7.getStyleClass().add("bpawn");
+        g2.getStyleClass().add("wpawn");
+        g7.getStyleClass().add("bpawn");
+        h2.getStyleClass().add("wpawn");
+        h7.getStyleClass().add("bpawn");
+
+        a1.getStyleClass().add("wrook");
+        a8.getStyleClass().add("brook");
+        h1.getStyleClass().add("wrook");
+        h8.getStyleClass().add("brook");
+
+        b1.getStyleClass().add("wknight");
+        b8.getStyleClass().add("bknight");
+        g1.getStyleClass().add("wknight");
+        g8.getStyleClass().add("bknight");
+
+        c1.getStyleClass().add("wbishop");
+        c8.getStyleClass().add("bbishop");
+        f1.getStyleClass().add("wbishop");
+        f8.getStyleClass().add("bbishop");
+
+        d1.getStyleClass().add("wking");
+        d8.getStyleClass().add("bking");
+
+        e1.getStyleClass().add("wqueen");
+        e8.getStyleClass().add("bqueen");
+    }
+
+    public void was_clicked(MouseEvent mouseEvent) {
+        ToggleButton test = new ToggleButton();
+
+        sp_a3.getChildren().add(test);
     }
 }
