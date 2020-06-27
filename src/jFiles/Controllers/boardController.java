6951ee89f2a269a74794f4b1679be0bf8b45f315
JephteyAdolphe <ja983@scarletmail.rutgers.edu>
2020-06-26 22:57:45 -0400

    Most pieces are now able to move correctly

--- src/jFiles/Controllers/boardController.java
+++ src/jFiles/Controllers/boardController.java
@@ -7,8 +7,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class boardController {
     public GridPane board_grid;
@@ -111,17 +110,6 @@ public class boardController {
 
     private Logic logic = new Logic();
 
-    private HashMap<String, ToggleButton> selected = new HashMap<>();
-    private ArrayList<ToggleButton> buttons = new ArrayList<>();
-
-    private void add_to_selected(String board_location, ToggleButton obj) {
-        selected.put(board_location, obj);
-    }
-
-    private void test() {
-        System.out.println(selected.get("a1").isSelected());
-    }
-
     @FXML
     public void initialize() {
 
@@ -135,15 +123,28 @@ public class boardController {
                 brook1, bknight1, bbisop1, bqueen,
                 bking, bbishop2, bknight2, brook2,
                 bpawn1, bpawn2, bpawn3, bpawn4,
-                bpawn5, bpawn6, bpawn7, bpawn8,
+                bpawn5, bpawn6, bpawn7, bpawn8
+        };
+
+        StackPane[] pane_list = {
+                sa1, sa2, sa3, sa4, sa5, sa6, sa7, sa8,
+                sb1, sb2, sb3, sb4, sb5, sb6, sb7, sb8,
+                sc1, sc2, sc3, sc4, sc5, sc6, sc7, sc8,
+                sd1, sd2, sd3, sd4, sd5, sd6, sd7, sd8,
+                se1, se2, se3, se4, se5, se6, se7, se8,
+                sf1, sf2, sf3, sf4, sf5, sf6, sf7, sf8,
+                sg1, sg2, sg3, sg4, sg5, sg6, sg7, sg8,
+                sh1, sh2, sh3, sh4, sh5, sh6, sh7, sh8,
         };
 
         for (int i = 0; i < loc_list.length; i++) {
             loc_list[i].getStylesheets().add("resources/views/css/stylesheet.css");
-            add_to_selected(String.valueOf(loc_list[i]), loc_list[i]);
-            buttons.add(loc_list[i]);
-            logic.pieces.add(loc_list[i]);
         }
+
+        logic.pieces.addAll(Arrays.asList(loc_list));
+        logic.stack_panes.addAll(Arrays.asList(pane_list));
+
+        // Add the matching graphics to the pieces (toggle buttons)
 
         wpawn1.getStyleClass().add("wpawn");
         bpawn1.getStyleClass().add("bpawn");
@@ -186,31 +187,23 @@ public class boardController {
         // Find a way to have a condition before allowing a toggle
 
         // See if that condition can be applied to all toggle buttons at once
-
-        // Can't allow multiple buttons to be toggled at once
-
-        // Make sure StackPane does not have a node before allowing piece to move to it
     }
 
-    public void sg3_clicked(MouseEvent mouseEvent) {
-
-        for (int i = 0; i < buttons.size(); i++) {
-            if (buttons.get(i).isSelected()) {
-                System.out.println(buttons.get(i));
-            }
-        }
-    }
+    // Methods for when the pieces are clicked
 
     public void brook1_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(brook1);
+        logic.create_destinations(brook1);
     }
 
     public void bknight1_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(bknight1);
+        logic.create_destinations(bknight1);
     }
 
     public void bbishop1_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(bbisop1);
+        logic.create_destinations(bbisop1);
     }
 
     public void bqueen_clicked(MouseEvent mouseEvent) {
@@ -223,42 +216,52 @@ public class boardController {
 
     public void bbishop2_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(bbishop2);
+        logic.create_destinations(bbishop2);
     }
 
     public void bknight2_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(bknight2);
+        logic.create_destinations(bknight2);
     }
 
     public void brook2_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(brook2);
+        logic.create_destinations(brook2);
     }
 
     public void wpawn1_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(wpawn1);
+        logic.create_destinations(wpawn1);
     }
 
     public void wrook1_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(wrook1);
+        logic.create_destinations(wrook1);
     }
 
     public void wpawn2_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(wpawn2);
+        logic.create_destinations(wpawn2);
     }
 
     public void wknight1_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(wknight1);
+        logic.create_destinations(wknight1);
     }
 
     public void wpawn3_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(wpawn3);
+        logic.create_destinations(wpawn3);
     }
 
     public void wbishop1_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(wbishop1);
+        logic.create_destinations(wbishop1);
     }
 
     public void wpawn4_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(wpawn4);
+        logic.create_destinations(wpawn4);
     }
 
     public void wqueen_clicked(MouseEvent mouseEvent) {
@@ -267,6 +270,7 @@ public class boardController {
 
     public void wpawn5_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(wpawn5);
+        logic.create_destinations(wpawn5);
     }
 
     public void wking_clicked(MouseEvent mouseEvent) {
@@ -275,58 +279,330 @@ public class boardController {
 
     public void wpawn6_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(wpawn6);
+        logic.create_destinations(wpawn6);
     }
 
     public void wbishop2_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(wbishop2);
+        logic.create_destinations(wbishop2);
     }
 
     public void wpawn7_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(wpawn7);
+        logic.create_destinations(wpawn7);
     }
 
     public void wknight2_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(wknight2);
+        logic.create_destinations(wknight2);
     }
 
     public void wpawn8_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(wpawn8);
+        logic.create_destinations(wpawn8);
     }
 
     public void wrook2_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(wrook2);
+        logic.create_destinations(wrook2);
     }
 
 
     public void bpawn1_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(bpawn1);
+        logic.create_destinations(bpawn1);
     }
 
     public void bpawn2_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(bpawn2);
+        logic.create_destinations(bpawn2);
     }
 
     public void bpawn3_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(bpawn3);
+        logic.create_destinations(bpawn3);
     }
 
     public void bpawn4_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(bpawn4);
+        logic.create_destinations(bpawn4);
     }
 
     public void bpawn5_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(bpawn5);
+        logic.create_destinations(bpawn5);
     }
 
     public void bpawn6_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(bpawn6);
+        logic.create_destinations(bpawn6);
     }
 
     public void bpawn7_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(bpawn7);
+        logic.create_destinations(bpawn7);
     }
 
     public void bpawn8_clicked(MouseEvent mouseEvent) {
         logic.untoggleOthers(bpawn8);
+        logic.create_destinations(bpawn8);
+    }
+
+    // Methods for when the pieces are clicked
+
+    public void sa8_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sa8);
+    }
+
+    public void sb8_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sb8);
+    }
+
+    public void sc8_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sc8);
+    }
+
+    public void sd8_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sd8);
+    }
+
+    public void se8_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(se8);
+    }
+
+    public void sf8_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sf8);
+    }
+
+    public void sg8_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sg8);
+    }
+
+    public void sh8_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sh8);
+    }
+
+    public void sa7_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sa7);
+    }
+
+    public void sa6_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sa6);
+    }
+
+    public void sa5_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sa5);
+    }
+
+    public void sa4_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sa4);
+    }
+
+    public void sa3_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sa3);
+    }
+
+    public void sa2_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sa2);
+    }
+
+    public void sa1_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sa1);
+    }
+
+    public void sb7_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sb7);
+    }
+
+    public void sb6_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sb6);
+    }
+
+    public void sb5_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sb5);
+    }
+
+    public void sb4_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sb4);
+    }
+
+    public void sb3_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sb3);
+    }
+
+    public void sb2_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sb2);
+    }
+
+    public void sb1_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sb1);
+    }
+
+    public void sc7_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sc7);
+    }
+
+    public void sc6_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sc6);
+    }
+
+    public void sc5_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sc5);
+    }
+
+    public void sc4_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sc4);
+    }
+
+    public void sc3_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sc3);
+    }
+
+    public void sc2_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sc2);
+    }
+
+    public void sc1_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sc1);
+    }
+
+    public void sd7_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sd7);
+    }
+
+    public void sd6_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sd6);
+    }
+
+    public void sd5_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sd5);
+    }
+
+    public void sd4_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sd4);
+    }
+
+    public void sd3_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sd3);
+    }
+
+    public void sd2_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sd2);
+    }
+
+    public void sd1_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sd1);
+    }
+
+    public void se7_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(se7);
+    }
+
+    public void se6_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(se6);
+    }
+
+    public void se5_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(se5);
+    }
+
+    public void se4_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(se4);
+    }
+
+    public void se3_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(se3);
+    }
+
+    public void se2_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(se2);
+    }
+
+    public void se1_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(se1);
+    }
+
+    public void sf7_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sf7);
+    }
+
+    public void sf6_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sf6);
+    }
+
+    public void sf5_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sf5);
+    }
+
+    public void sf4_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sf4);
+    }
+
+    public void sf3_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sf3);
+    }
+
+    public void sf2_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sf2);
+    }
+
+    public void sf1_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sf1);
+    }
+
+    public void sg7_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sg7);
+    }
+
+    public void sg6_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sg6);
+    }
+
+    public void sg5_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sg5);
+    }
+
+    public void sg4_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sg4);
+    }
+
+    public void sg3_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sg3);
+    }
+
+    public void sg2_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sg2);
+    }
+
+    public void sg1_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sg1);
+    }
+
+    public void sh7_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sh7);
+    }
+
+    public void sh6_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sh6);
+    }
+
+    public void sh5_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sh5);
+    }
+
+    public void sh4_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sh4);
+    }
+
+    public void sh3_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sh3);
+    }
+
+    public void sh2_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sh2);
+    }
+
+    public void sh1_clicked(MouseEvent mouseEvent) {
+        logic.check_for_toggled_piece(sh1);
     }
 }
--- src/jFiles/Logic.java
+++ src/jFiles/Logic.java
@@ -1,13 +1,16 @@
 package jFiles;
 
 import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Logic {
 
     public ArrayList<ToggleButton> pieces = new ArrayList<>();
+    public ArrayList<StackPane> stack_panes = new ArrayList<>();
+    private ArrayList<StackPane> possible_destinations = new ArrayList<>();
 
     public void untoggleOthers(ToggleButton picked) {
         for (int i = 0; i < pieces.size(); i++) {
@@ -15,6 +18,290 @@ public class Logic {
                 pieces.get(i).setSelected(false);
             }
         }
-        System.out.println("untoggled");
     }
-}
+
+    // Checks to see what piece, if any, is toggled
+
+    public void check_for_toggled_piece(StackPane sp) {
+
+        ToggleButton toggled_piece = null;
+
+        for (int i = 0; i < pieces.size(); i++) {
+            if (pieces.get(i).isSelected()) {
+                toggled_piece = pieces.get(i);
+                break;
+            }
+        }
+        // Checks if the pressed StackPane is in the list of valid destination panes for the selected piece
+        if (possible_destinations.contains(sp)) {
+            sp.getChildren().add(toggled_piece);
+            assert toggled_piece != null;
+            toggled_piece.setSelected(false);
+        }
+    }
+
+    // Contains switch case for determining how to make the list of valid destinations
+
+    public void create_destinations(ToggleButton pressed) {
+        possible_destinations.clear();
+        String currentPane = pressed.getParent().getId();
+
+        int row = 0;
+        int col = 0;
+
+        for (int i = 0; i < stack_panes.size(); i++) {
+            if (String.valueOf(stack_panes.get(i).getId()).equals(currentPane)) {
+                row = GridPane.getRowIndex(stack_panes.get(i));
+                col = GridPane.getColumnIndex(stack_panes.get(i));
+
+                System.out.println(String.format("(%d, %d)", row, col));
+                break;
+            }
+        }
+
+        switch (String.valueOf(pressed.getId())) {
+            case "bpawn1":
+            case "bpawn2":
+            case "bpawn3":
+            case "bpawn4":
+            case "bpawn5":
+            case "bpawn6":
+            case "bpawn7":
+            case "bpawn8":
+            case "wpawn1":
+            case "wpawn2":
+            case "wpawn3":
+            case "wpawn4":
+            case "wpawn5":
+            case "wpawn6":
+            case "wpawn7":
+            case "wpawn8":
+                pawn_pressed(pressed, row, col);
+                break;
+
+            case "brook1":
+            case "brook2":
+            case "wrook1":
+            case "wrook2":
+                rook_pressed(row, col);
+                break;
+
+            case "bknight1":
+            case "bknight2":
+            case "wknight1":
+            case "wknight2":
+                knight_pressed(row, col);
+                break;
+
+            case "bbishop1":
+            case "bbishop2":
+            case "wbishop1":
+            case "wbishop2":
+                bishop_pressed(row, col);
+                break;
+        }
+    }
+
+    private void pawn_pressed(ToggleButton pressed, int row, int col) {
+        for (int i = 0; i < stack_panes.size(); i++) {
+            int tempCol = GridPane.getColumnIndex(stack_panes.get(i));
+            int tempRow = GridPane.getRowIndex(stack_panes.get(i));
+
+            // Generates a list depending on which color pawn is in play
+
+            if (String.valueOf(pressed.getId()).contains("bpawn")) {
+
+                if (tempCol == col && tempRow == row + 1 && stack_panes.get(i).getChildren().isEmpty()) {
+                    possible_destinations.add(stack_panes.get(i));
+                    break;
+                }
+            }
+
+            if (String.valueOf(pressed.getId()).contains("wpawn")) {
+
+                if (tempCol == col && tempRow == row - 1 && stack_panes.get(i).getChildren().isEmpty()) {
+                    possible_destinations.add(stack_panes.get(i));
+                    break;
+                }
+            }
+        }
+    }
+
+        private void rook_pressed (int row, int col){
+            // Row by row check for open spots and rook cannot jump over pieces so we break when we hit the first tile
+            // with a piece on it
+
+            for (int r = row + 1; r <= 7; r++) {
+                int rowLimit = 0;
+
+                for (int i = 0; i < stack_panes.size(); i++) {
+                    if (GridPane.getColumnIndex(stack_panes.get(i)) == col && GridPane.getRowIndex(stack_panes.get(i)) == r && stack_panes.get(i).getChildren().isEmpty()) {
+                        possible_destinations.add(stack_panes.get(i));
+                        break;
+                    } else if (GridPane.getColumnIndex(stack_panes.get(i)) == col && GridPane.getRowIndex(stack_panes.get(i)) == r && !stack_panes.get(i).getChildren().isEmpty()) {
+                        rowLimit = 1;
+                        break;
+                    }
+                }
+                if (rowLimit == 1) break;
+            }
+
+            // Col by col check for open spots and rook cannot jump over pieces so we break when we hit the first tile
+            // with a piece on it
+
+            for (int c = col + 1; c <= 7; c++) {
+                int colLimit = 0;
+
+                for (int i = 0; i < stack_panes.size(); i++) {
+                    if (GridPane.getRowIndex(stack_panes.get(i)) == row && GridPane.getColumnIndex(stack_panes.get(i)) == c && stack_panes.get(i).getChildren().isEmpty()) {
+                        possible_destinations.add(stack_panes.get(i));
+                        break;
+                    } else if (GridPane.getRowIndex(stack_panes.get(i)) == row && GridPane.getColumnIndex(stack_panes.get(i)) == c && !stack_panes.get(i).getChildren().isEmpty()) {
+                        colLimit = 1;
+                        break;
+                    }
+                }
+                if (colLimit == 1) break;
+            }
+            // Row by row in opposite direction
+            for (int r = row - 1; r >= 0; r--) {
+                int rowLimit = 0;
+
+                for (int i = 0; i < stack_panes.size(); i++) {
+                    if (GridPane.getColumnIndex(stack_panes.get(i)) == col && GridPane.getRowIndex(stack_panes.get(i)) == r && stack_panes.get(i).getChildren().isEmpty()) {
+                        possible_destinations.add(stack_panes.get(i));
+                        break;
+                    } else if (GridPane.getColumnIndex(stack_panes.get(i)) == col && GridPane.getRowIndex(stack_panes.get(i)) == r && !stack_panes.get(i).getChildren().isEmpty()) {
+                        rowLimit = 1;
+                        break;
+                    }
+                }
+                if (rowLimit == 1) break;
+            }
+            // Col by col in opposite direction
+            for (int c = col - 1; c >= 0; c--) {
+                int colLimit = 0;
+
+                for (int i = 0; i < stack_panes.size(); i++) {
+                    if (GridPane.getRowIndex(stack_panes.get(i)) == row && GridPane.getColumnIndex(stack_panes.get(i)) == c && stack_panes.get(i).getChildren().isEmpty()) {
+                        possible_destinations.add(stack_panes.get(i));
+                        break;
+                    } else if (GridPane.getRowIndex(stack_panes.get(i)) == row && GridPane.getColumnIndex(stack_panes.get(i)) == c && !stack_panes.get(i).getChildren().isEmpty()) {
+                        colLimit = 1;
+                        break;
+                    }
+                }
+                if (colLimit == 1) break;
+            }
+        }
+
+        private void knight_pressed(int row, int col) {
+
+            // Checks all possible spots for the knight (regardless of its color) to land
+
+            for (int i = 0; i < stack_panes.size(); i++) {
+                if (GridPane.getRowIndex(stack_panes.get(i)) == row + 1 && GridPane.getColumnIndex(stack_panes.get(i)) == col + 2) {
+                    possible_destinations.add(stack_panes.get(i));
+                }
+                if (GridPane.getRowIndex(stack_panes.get(i)) == row + 1 && GridPane.getColumnIndex(stack_panes.get(i)) == col - 2) {
+                    possible_destinations.add(stack_panes.get(i));
+                }
+                if (GridPane.getRowIndex(stack_panes.get(i)) == row - 1 && GridPane.getColumnIndex(stack_panes.get(i)) == col + 2) {
+                    possible_destinations.add(stack_panes.get(i));
+                }
+                if (GridPane.getRowIndex(stack_panes.get(i)) == row - 1 && GridPane.getColumnIndex(stack_panes.get(i)) == col - 2) {
+                    possible_destinations.add(stack_panes.get(i));
+                }
+                if (GridPane.getRowIndex(stack_panes.get(i)) == row + 2 && GridPane.getColumnIndex(stack_panes.get(i)) == col + 1) {
+                    possible_destinations.add(stack_panes.get(i));
+                }
+                if (GridPane.getRowIndex(stack_panes.get(i)) == row + 2 && GridPane.getColumnIndex(stack_panes.get(i)) == col - 1) {
+                    possible_destinations.add(stack_panes.get(i));
+                }
+                if (GridPane.getRowIndex(stack_panes.get(i)) == row - 2 && GridPane.getColumnIndex(stack_panes.get(i)) == col + 1) {
+                    possible_destinations.add(stack_panes.get(i));
+                }
+                if (GridPane.getRowIndex(stack_panes.get(i)) == row - 2 && GridPane.getColumnIndex(stack_panes.get(i)) == col - 1) {
+                    possible_destinations.add(stack_panes.get(i));
+                }
+            }
+        }
+
+        private void bishop_pressed(int row, int col) {
+            int r = row;
+            int c = col;
+
+            // bottom right check
+            while(true) {
+                int limit = 0;
+                for (int i = 0; i < stack_panes.size(); i++) {
+                    if (GridPane.getRowIndex(stack_panes.get(i)) == r + 1 && GridPane.getColumnIndex(stack_panes.get(i)) == c + 1 && stack_panes.get(i).getChildren().isEmpty()) {
+                        possible_destinations.add(stack_panes.get(i));
+                    } else if (GridPane.getRowIndex(stack_panes.get(i)) == r + 1 && GridPane.getColumnIndex(stack_panes.get(i)) == c + 1 && !stack_panes.get(i).getChildren().isEmpty()) {
+                        limit = 1;
+                    }
+                }
+                if (limit == 1) break;
+                r++;
+                c++;
+                if (c == 9) break;
+            }
+            r = row;
+            c = col;
+
+            // bottom left check
+            while(true) {
+                int limit = 0;
+                for (int i = 0; i < stack_panes.size(); i++) {
+                    if (GridPane.getRowIndex(stack_panes.get(i)) == r + 1 && GridPane.getColumnIndex(stack_panes.get(i)) == c - 1 && stack_panes.get(i).getChildren().isEmpty()) {
+                        possible_destinations.add(stack_panes.get(i));
+                    } else if (GridPane.getRowIndex(stack_panes.get(i)) == r + 1 && GridPane.getColumnIndex(stack_panes.get(i)) == c - 1 && !stack_panes.get(i).getChildren().isEmpty()) {
+                        limit = 1;
+                    }
+                }
+                if (limit == 1) break;
+                r++;
+                c--;
+                if (c == -1) break;
+            }
+
+            r = row;
+            c = col;
+
+            // top right check
+            while(true) {
+                int limit = 0;
+                for (int i = 0; i < stack_panes.size(); i++) {
+                    if (GridPane.getRowIndex(stack_panes.get(i)) == r - 1 && GridPane.getColumnIndex(stack_panes.get(i)) == c + 1 && stack_panes.get(i).getChildren().isEmpty()) {
+                        possible_destinations.add(stack_panes.get(i));
+                    } else if (GridPane.getRowIndex(stack_panes.get(i)) == r - 1 && GridPane.getColumnIndex(stack_panes.get(i)) == c + 1 && !stack_panes.get(i).getChildren().isEmpty()) {
+                        limit = 1;
+                    }
+                }
+                if (limit == 1) break;
+                r--;
+                c++;
+                if (c == 9) break;
+            }
+
+            r = row;
+            c = col;
+
+            // top left check
+            while(true) {
+                int limit = 0;
+                for (int i = 0; i < stack_panes.size(); i++) {
+                    if (GridPane.getRowIndex(stack_panes.get(i)) == r - 1 && GridPane.getColumnIndex(stack_panes.get(i)) == c - 1 && stack_panes.get(i).getChildren().isEmpty()) {
+                        possible_destinations.add(stack_panes.get(i));
+                    } else if (GridPane.getRowIndex(stack_panes.get(i)) == r - 1 && GridPane.getColumnIndex(stack_panes.get(i)) == c - 1 && !stack_panes.get(i).getChildren().isEmpty()) {
+                        limit = 1;
+                    }
+                }
+                if (limit == 1) break;
+                r--;
+                c--;
+                if (c == -1) break;
+            }
+        }
+    }
+
